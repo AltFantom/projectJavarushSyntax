cf70f4d72ec942cc369fa7a5868ced14fad53772
AltFantom <altFantom@yandex.ru>
2022-08-14 17:09:44 +0300

    complete first mode

--- src/DAO.java
+++ src/DAO.java
@@ -1,6 +1,4 @@
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class DAO {
@@ -13,10 +11,11 @@ public class DAO {
         System.out.println("""
                 Что ты хочешь сделать с файлом?\s
                 1. Зашифровать\s
-                2. Расшифровать""");
+                2. Расшифровать\s
+                3. Расшифровать подбором ключа""");
         int yourChoice = Integer.parseInt(sc.nextLine());
-        switch(yourChoice){
-            case 1:
+        switch (yourChoice) {
+            case 1 -> {
                 System.out.println("Название файла c исходным текстом");
                 fileFrom = sc.nextLine();
                 System.out.println("В какой файл поместить зашифрованный текст");
@@ -24,56 +23,90 @@ public class DAO {
                 System.out.println("Выбери сдвиг");
                 key = new Key(Integer.parseInt(sc.nextLine()));
                 toEncrypt(fileFrom, fileTo, key);
-                break;
-            case 2:
+            }
+            case 2 -> {
                 System.out.println("Название файла с зашифрованным текстом");
                 fileFrom = sc.nextLine();
                 System.out.println("В какой файл поместить расшифрованный текст");
                 fileTo = sc.nextLine();
                 System.out.println("Задай сдвиг ключа");
                 key = new Key(Integer.parseInt(sc.nextLine()));
-                toDecode(fileFrom, fileTo, key);
-                break;
-            default:
-                System.out.println("Такой команды не существует");
+                toEncrypt(fileFrom, fileTo, key);
+            }
+            case 3 -> {
+                System.out.println("Название файла c исходным текстом");
+                fileFrom = sc.nextLine();
+                System.out.println("В какой файл поместить зашифрованный текст");
+                fileTo = sc.nextLine();
+                key = new Key(bruteForce(fileFrom));
+                System.out.println("Ключ шифра — " + key.getShift());
+                toEncrypt(fileFrom, fileTo, key);
+            }
+            default -> System.out.println("Такой команды не существует");
         }
         sc.close();
     }
     public static void toEncrypt(String fileFrom, String fileTo, Key key){
         try(FileReader fileReader = new FileReader(fileFrom);
             FileWriter fileWriter = new FileWriter(fileTo)){
+            int beginKey = key.getShift();
+            char symbol;
             while (fileReader.ready()){
-                char ch = (char) (fileReader.read() + key.getShift());
-                while (!isRussCharOrPunctuationMark(ch)){
-                    ch += 1;
+                symbol = (char) fileReader.read();
+                while (key.getShift() != 0){
+                    if(key.getShift() > 0){
+                        do{
+                            symbol += 1;
+                        } while (isCharWhichNeedByCondition(symbol));
+                        key.setShift(key.getShift() - 1);
+                    }
+                    else if(key.getShift() < 0){
+                        do{
+                            symbol -= 1;
+                        } while (isCharWhichNeedByCondition(symbol));
+                        key.setShift(key.getShift() + 1);
+                    }
                 }
-                fileWriter.write(ch);
+                fileWriter.write(symbol);
+                key.setShift(beginKey);
             }
         }
         catch(IOException e){
             System.out.println(e.getMessage());
         }
     }
-    public static void toDecode(String fileFrom, String fileTo, Key key){
-        try(FileReader fileReader = new FileReader(fileFrom);
-            FileWriter fileWriter = new FileWriter(fileTo)){
-            while (fileReader.ready()){
-                char ch = (char) (fileReader.read() + key.getShift());
-                while (!isRussCharOrPunctuationMark(ch)){
-                    ch -= 1;
+    public static boolean isCharWhichNeedByCondition(char ch){
+        return ch != 32 && ch != 33 && ch != 34 && ch != 44 &&
+                ch != 45 && ch != 46 && ch != 58 && ch != 63 && (ch < 1040 || ch > 1103);
+    }
+    public static int bruteForce(String fileFrom){
+        try(FileInputStream fileInputStream = new FileInputStream(fileFrom);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileFrom),
+                    fileInputStream.available())){
+            bufferedReader.mark(200);
+            int counter = 0;
+            char firstChar;
+            char secondChar = 0;
+            int AllCharsInText = fileInputStream.available();
+            for (int key = 1; key < 73; key++) {
+                bufferedReader.reset();
+                while(fileInputStream.available() > 0){
+                    if(AllCharsInText / 115 > counter){
+                        firstChar = secondChar;
+                        secondChar = (char)fileInputStream.read();
+                        if ((firstChar == 46 || firstChar == 33 || firstChar == 44 ||
+                                firstChar == 63) && secondChar == 32){
+                            counter += 1;
+                        }
+                    }
+                    else
+                        return key;
                 }
-                fileWriter.write(ch);
             }
         }
         catch(IOException e){
             System.out.println(e.getMessage());
         }
-    }
-    public static boolean isRussCharOrPunctuationMark(char ch){
-        if(ch == 32 || ch == 33 || ch == 34 || ch == 44 ||
-                ch == 45 || ch == 46 || ch == 58 || ch == 63 || (ch > 1039 && ch < 1104) ){
-            return true;
-        }
-        return false;
+        return 9999;
     }
 }
--- src/Key.java
+++ src/Key.java
@@ -1,32 +1,13 @@
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class Key
 {
     private int shift;
-
     public int getShift() {
         return shift;
     }
-
     public void setShift(int shift) {
         this.shift = shift;
     }
-
     public Key(int shift){
         this.shift = shift;
     }
-
-    public static void main(String[] args) {
-        System.out.println((int)'"');
-    }
-
-    /*если запятая и пробел или точка и пробел встречаются на текст его символов опред кол во раз,
-     * то мы правильно подобрали ключ к тексту
-     * 1. сделать сплит по тексту разделающий значения его по точкепробелу и любой символ из
-     * русского алфавита с большой буквы
-     * 2. также проверить встр ли в тексте запятая и пробел на
-     * определенное количество символов в тексте
-     * 3. */
 }
