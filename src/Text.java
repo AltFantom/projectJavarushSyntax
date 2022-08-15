92b0dceaf255bbc5ffa5e6f87e6dcf931c8956d8
AltFantom <AltF4ntom@yandex.ru>
2022-08-15 17:41:27 +0300

    almost done

--- src/Text.java
+++ src/Text.java
@@ -1,52 +1,57 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class DAO {
-
+public class Text {
+private String fileFrom;
+private String fileTo;
+private Key key;
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String fileFrom;
-        String fileTo;
-        Key key;
-        System.out.println("""
-                Что ты хочешь сделать с файлом?\s
-                1. Зашифровать\s
-                2. Расшифровать\s
-                3. Расшифровать подбором ключа""");
-        int yourChoice = Integer.parseInt(sc.nextLine());
-        switch (yourChoice) {
-            case 1 -> {
-                System.out.println("Название файла c исходным текстом");
-                fileFrom = sc.nextLine();
-                System.out.println("В какой файл поместить зашифрованный текст");
-                fileTo = sc.nextLine();
-                System.out.println("Выбери сдвиг");
-                key = new Key(Integer.parseInt(sc.nextLine()));
-                toEncrypt(fileFrom, fileTo, key);
-            }
-            case 2 -> {
-                System.out.println("Название файла с зашифрованным текстом");
-                fileFrom = sc.nextLine();
-                System.out.println("В какой файл поместить расшифрованный текст");
-                fileTo = sc.nextLine();
-                System.out.println("Задай сдвиг ключа");
-                key = new Key(Integer.parseInt(sc.nextLine()));
-                toEncrypt(fileFrom, fileTo, key);
-            }
-            case 3 -> {
-                System.out.println("Название файла c исходным текстом");
-                fileFrom = sc.nextLine();
-                System.out.println("В какой файл поместить зашифрованный текст");
-                fileTo = sc.nextLine();
-                key = new Key(bruteForce(fileFrom));
-                System.out.println("Ключ шифра — " + key.getShift());
-                toEncrypt(fileFrom, fileTo, key);
-            }
-            default -> System.out.println("Такой команды не существует");
-        }
-        sc.close();
+        Text text = new Text();
+        text.workWithConsole();
     }
-    public static void toEncrypt(String fileFrom, String fileTo, Key key){
+        public void workWithConsole(){
+            Scanner sc = new Scanner(System.in);
+            int yourChoice = -1;
+            while (yourChoice != 0) {
+                System.out.println("""
+                        What do you want to do with file?\s
+                        1. Encrypt/Decode\s
+                        2. Decode with bruteForce
+                        3. Exit""");
+                try {
+                    yourChoice = Integer.parseInt(sc.nextLine());
+                }
+                catch(NumberFormatException ignored){
+                }
+                switch (yourChoice) {
+                    case 1 -> {
+                        System.out.println("Name FileFrom");
+                        fileFrom = sc.nextLine();
+                        System.out.println("Name FileTo");
+                        fileTo = sc.nextLine();
+                        System.out.println("ChooseShift");
+                        key = new Key(Integer.parseInt(sc.nextLine()));
+                        toEncrypt(fileFrom, fileTo, key);
+                    }
+                    case 2 -> {
+                        System.out.println("Name FileFrom");
+                        fileFrom = sc.nextLine();
+                        System.out.println("Name FileTo");
+                        fileTo = sc.nextLine();
+                        key = new Key(bruteForce(fileFrom));
+                        if (key.getShift() == 404) {
+                            System.out.println("Program doesn't work or you do something wrong");
+                        }
+                        System.out.println("KeyShift — " + key.getShift());
+                        toEncrypt(fileFrom, fileTo, key);
+                    }
+                    case 3 -> yourChoice = 0;
+                    default -> System.out.println("command doesn't exist");
+                }
+            }
+            sc.close();
+        }
+    public void toEncrypt(String fileFrom, String fileTo, Key key){
         try(FileReader fileReader = new FileReader(fileFrom);
             FileWriter fileWriter = new FileWriter(fileTo)){
             int beginKey = key.getShift();
@@ -75,22 +80,21 @@ public class DAO {
             System.out.println(e.getMessage());
         }
     }
-    public static boolean isCharWhichNeedByCondition(char ch){
+    public boolean isCharWhichNeedByCondition(char ch){
         return ch != 32 && ch != 33 && ch != 34 && ch != 44 &&
                 ch != 45 && ch != 46 && ch != 58 && ch != 63 && (ch < 1040 || ch > 1103);
     }
-    public static int bruteForce(String fileFrom){
+    public int bruteForce(String fileFrom){
         try(FileReader fileReader = new FileReader(fileFrom);
         BufferedReader bufferedReader = new BufferedReader(fileReader)){
             StringBuilder text = new StringBuilder("");
             char symbol;
             for (int key = 1; key < 73; key++) {
-                int keyBegin = key;
                 int anotherKey;
                 bufferedReader.mark(1000);
                 text.delete(0, text.length());
                 while(bufferedReader.ready()){
-                    anotherKey = keyBegin;
+                    anotherKey = key;
                     symbol = (char)(bufferedReader.read());
                     while(anotherKey != 0) {;
                         do {
@@ -109,6 +113,6 @@ public class DAO {
         catch(IOException e){
             System.out.println(e.getMessage());
         }
-        return 9999;
+        return 404;
     }
 }
